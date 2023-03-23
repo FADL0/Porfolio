5eba0e35355074c2c1aecad17bb7c2c6480ca3c4
FADL0 <haddoufa10@gmail.com>
2023-03-23 16:57:51 +0100

    Sync

--- Background.js
+++ Background.js
@@ -1,23 +1,52 @@
 var body = document.getElementsByTagName('body')[0];
 body.style.backgroundColor = 'black';
-
+body.style.transitionDuration = '2s'
 // trigger this function every time the user scrolls
+
+
+
+function myFunction(x) {
+if (x.matches) { // If media query matches
 window.onscroll = function (event) {
+    var scroll = window.pageYOffset;
+
+    if (scroll < 950) {
+        // mainpage
+        body.style.backgroundColor = 'black';
+         
+    }  else if (scroll >= 1500 && scroll < 2500) {
+        // yellow
+        body.style.backgroundColor = '#238c2a'
+    } else if (scroll >= 2500 && scroll < 5000) {
+        // blue
+       
+        body.style.backgroundColor = 'black';
+    } else if (scroll >= 5000 && scroll < 6000) {
+        // orange
+        body.style.backgroundColor = '#93C572';
+    } else {
+        // purple
+        body.style.backgroundColor = 'black';
+    }
+    
+}
+} else  {
+    window.onscroll = function (event) {
     var scroll = window.pageYOffset;
 
     if (scroll < 1250) {
         // mainpage
         body.style.backgroundColor = 'black';
-         body.style.transitionDuration = '2s'
-    } else if (scroll >= 1250 && scroll < 2000) {
+     
+    }  else if (scroll >= 1250 && scroll < 2000) {
         // page2
         body.style.backgroundColor = '#238c2a'
-    } else if (scroll >= 2000 && scroll < 3400) {
+    } else if (scroll >= 2000 && scroll < 3850) {
         // page3
        
         body.style.backgroundColor = 'black';
-        body.style.transitionDuration = '2s'
-    } else if (scroll >= 3400 && scroll < 4000) {
+    
+    } else if (scroll >= 3850 && scroll < 6000) {
         // page4
         body.style.backgroundColor = '#93C572';
     
@@ -29,39 +58,48 @@ window.onscroll = function (event) {
 }
 
 
-function myFunction(x) {
-if (x.matches) { // If media query matches
-window.onscroll = function (event) {
-    var scroll = window.pageYOffset;
-
-    if (scroll < 950) {
-        // mainpage
-        body.style.backgroundColor = 'black';
-         body.style.transitionDuration = '2s'
-    } else if (scroll >= 300 && scroll < 1800) {
-        // yellow
-        body.style.backgroundColor = '#238c2a'
-    } else if (scroll >= 600 && scroll < 1200) {
-        // blue
-       
-        body.style.backgroundColor = 'black';
-    } else if (scroll >= 1200 && scroll < 1800) {
-        // orange
-        body.style.backgroundColor = 'black';
-    } else if (scroll >= 1800 && scroll < 3000) {
-        // red
-        body.style.backgroundColor = 'black';
-    } else {
-        // purple
-        body.style.backgroundColor = 'black';
-    }
-    
-}
-} else {
-
 }
 }
 
 var x = window.matchMedia("(max-width: 600px)")
 myFunction(x) // Call listener function at run time
 x.addListener(myFunction) // Attach listener function on state changes
+
+/* Burger */
+let menu = document.querySelector(".imlosingit");
+let menudisplay = document.querySelector(".demontime");
+
+
+menu.addEventListener('click', function(){
+    
+    menudisplay.classList.toggle('demontimesupreme');
+    menu.classList.toggle('orami');
+  
+})
+
+
+let home = document.querySelector(".home")
+let homechanges = document.querySelector(".homechanges")
+let about = document.querySelector(".About")
+let Exp = document.querySelector(".Exp")
+let Resum = document.querySelector(".Resum")
+home.addEventListener('click',function(){
+    homechanges.classList.toggle('homechanges2')
+})
+
+home.addEventListener('click' , function(){
+    menudisplay.classList.remove('demontimesupreme');
+    menu.classList.remove('orami');
+})
+about.addEventListener("click", ()=>{
+    menudisplay.classList.remove('demontimesupreme');
+    menu.classList.remove('orami');
+})
+Exp.addEventListener("click", ()=>{
+    menudisplay.classList.remove('demontimesupreme');
+    menu.classList.remove('orami');
+})
+Resum.addEventListener('click', function(){
+    menudisplay.classList.remove('demontimesupreme');
+    menu.classList.remove('orami');
+})
